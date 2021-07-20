a5811ee6c5805c4aeba2bbe1b1666c7bdb6ac3fd
Lacey Umamoto <lcumamoto@gmail.com>
2021-07-20 00:20:07 -0700

    update about

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -12,17 +12,17 @@ const About = () => {
           <Text>
             My pronouns are she/her/hers. I recently graduated from the
             University of California, San Diego in December 2020 with a{" "}
-            <span className="text-primary">B.S. in Computer Science</span>, and I am
-            currently searching for full-time software engineering
+            <span className="text-primary">B.S. in Computer Science</span>, and
+            I am currently searching for full-time software engineering
             opportunities.
-            <br />I have experience in web and mobile fullstack development, as
-            well as a bit of game development. I enjoy collaborating with others
-            and learning new technologies, which is why I enjoy participating in{" "}
+            <br />I have experience in web, mobile, and game
+            development, and I enjoy creating projects both on my own and with
+            others at{" "}
             <a href="https://devpost.com/lumamoto">
               <b className="text-primary lined-link">hackathons</b>
             </a>
-            . As a developer, my goal is to write beautiful code and deliver
-            beautiful products.
+            . In my free time, I love working on fun side projects, learning new
+            technologies, discovering new music, and cooking funky vegan food.
             <br />
           </Text>
           {/* <ResumeButton href="Lacey-Umamoto-Resume.pdf" target="_blank">
