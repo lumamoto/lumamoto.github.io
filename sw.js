b40439cae67397d63ccead1172cfd55ad3841129
Lacey Umamoto <lcumamoto@gmail.com>
2021-07-19 23:07:49 -0700

    Updates

--- sw.js
+++ sw.js
@@ -33,11 +33,11 @@ self.__precacheManifest = [
     "url": "framework-83e11af5cf111be5ad89.js"
   },
   {
-    "url": "app-1772b4936fe80b931aed.js"
+    "url": "app-6d93a7142183c95d10ce.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "e6359dc99d0b530ffdbb129dfe962cfc"
+    "revision": "59e6e483173cec738d262330db14c09e"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-7542ec632975394c09bc.js"
@@ -140,7 +140,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-1772b4936fe80b931aed.js`))) {
+  if (!resources || !(await caches.match(`/app-6d93a7142183c95d10ce.js`))) {
     return await fetch(event.request)
   }
 
