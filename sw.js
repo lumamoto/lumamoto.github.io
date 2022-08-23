1f5c292003984d08e71959ba54f74de92e996b89
Lacey Umamoto <lcumamoto@gmail.com>
2022-08-22 22:26:49 -0700

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3290cfe811556bfbc44f.js"
+    "url": "webpack-runtime-39cb25a766d48e6e2d31.js"
   },
   {
     "url": "framework-8396579dd65f14c8d0f9.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "35922325c70f9677b3a23cd7890c7dae"
+    "revision": "9c2a9d1b69c6c4ec25302d636cdeba6b"
   },
   {
     "url": "polyfill-f65b908849003923ae65.js"
